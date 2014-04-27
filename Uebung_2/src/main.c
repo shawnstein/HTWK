144f7cc1884244d7a3b490ac509af640b3750d2e
shawnstein <sherman@viacobra.de>
2014-04-27 11:41:29 +0200

    Update main.c
    Formatierung

--- Uebung_2/src/main.c
+++ Uebung_2/src/main.c
@@ -13,8 +13,8 @@ int round_f(float x) {
 
 /* Speichern der Zahlen im Interval nach Schritten und Berechnung der log-Funktionen aus diesen */
 void operate(u_int from, u_int to, float steps, u_int operation) {	
-	u_int index = 0;												/* Indexierung */
-	float loop;													/* Init der Schleifevar nach C89-Standard */
+	u_int index = 0;								/* Indexierung */
+	float loop;									/* Init der Schleifevar nach C89-Standard */
 
 	/* Anlegen mehrer Zeiger-Arrays aus der Groesse des Intervals */
 	struct element *ptr = (struct element *) calloc(round_f((to - from) / steps) , sizeof(struct element));
@@ -23,40 +23,40 @@ void operate(u_int from, u_int to, float steps, u_int operation) {
 		exit(1);
     }
 	else {
-		for(loop = 0; loop < (to - from); loop += steps) {				/* Schleife in Dezimalschritten*/	
-			ptr[index].num = (loop) + (float)from; 					/* Addition des Startintervals zum aktuellen Schleifenindex */					
+		for(loop = 0; loop < (to - from); loop += steps) {			/* Schleife in Dezimalschritten*/	
+			ptr[index].num = (loop) + (float)from; 				/* Addition des Startintervals zum aktuellen Schleifenindex */					
 			switch(operation) {
 				case LG: ptr[index].res = log(ptr[index].num); break; 	/* Berechnung */			
 				case LN: ptr[index].res = log10(ptr[index].num); break;	/* Berechnung */		
 				case LD: ptr[index].res = log2(ptr[index].num); break; 	/* Berechnung */					
-			} index++;													/* Indexierung um 1 erhoehen */	
+			} index++;							/* Indexierung um 1 erhoehen */	
 		} 
-		print_tbl(index-1, ptr);										/* Ausgabe der Datensturktur inklusive der Indexierung */							
-		free(ptr);														/* Freigabe des beanspruchten Speichers */		
+		print_tbl(index-1, ptr);						/* Ausgabe der Datensturktur inklusive der Indexierung */							
+		free(ptr);								/* Freigabe des beanspruchten Speichers */		
 	}
 }	
 
 /* Ausgabe der Datensturktur inklusive der Indexierung */
 void print_tbl(u_int index, struct element *elem) {					
-	char c;															/* Var zur Speicherung des Tastatur-Interrupts */
-	u_int fast_index = 1; 											/* Neuordnung des Indexes, beginnend mit 1 (nicht 0) */
+	char c;										/* Var zur Speicherung des Tastatur-Interrupts */
+	u_int fast_index = 1; 								/* Neuordnung des Indexes, beginnend mit 1 (nicht 0) */
 	
 	printf("\n#INFO# - Zeige Ergebnisse... \n");						
 	for(u_int i = 0; i < index; i++) {						
 		printf("%d. %f - %f\t|", fast_index, elem[i].num, elem[i].res); 						
 		if(!(fast_index % 10) && (fast_index != 0)) {								
-			c = getchar(); 											/* Speichern des Tastaturinterrupts */
-			if(c == '\n') printf("\n-------------\n");				/* Vergleich der Tastatureingabe */			
-		} fast_index++; 											/* Indexierung um 1 erhoehen */
+			c = getchar(); 							/* Speichern des Tastaturinterrupts */
+			if(c == '\n') printf("\n-------------\n");			/* Vergleich der Tastatureingabe */			
+		} fast_index++; 							/* Indexierung um 1 erhoehen */
 	} printf("\n=============\n");								
 }
 
 int main() {
-	u_int opt = -1, from, to;										/* Option, Interval-Start, Interval-Ziel */																	
-	u_char loop;													/* Init Schleifen-Var */									
-	float steps;													/* Schrittfolge */				
+	u_int opt = -1, from, to;							/* Option, Interval-Start, Interval-Ziel */																	
+	u_char loop;									/* Init Schleifen-Var */									
+	float steps;									/* Schrittfolge */				
 
-	system("clear");												/* Saeubern der Konsole */								
+	system("clear");								/* Saeubern der Konsole */								
 	printf("\nlog-Funktionen by Sandor Farbas\n");						
 	while(opt) {	
 		loop = 0;									
@@ -80,7 +80,7 @@ int main() {
 				}
 				if(loop == 2) {
 					printf("\n#INFO# - Bitte Schrittfolge angeben: ");
-					scanf("%f", &steps);			 /* segfault nach scanf */			
+					scanf("%f", &steps);			 	/* segfault nach scanf */			
 					(steps == 0) ? printf("\n#ERROR# - Ein Problem wurde festgestellt. "
 										  "Schrittfolge ist 0."
 										  ) : (loop += 1); 
