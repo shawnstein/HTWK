eb18c57721768a315200d59037a5da2d5e460080
shawnstein <sherman@viacobra.de>
2014-05-09 16:13:58 +0200

    neues projekt

---
+++ ADS_Prog1/main.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+typedef unsigned int u_int;
+
+void fill(u_int *array, u_int len) {
+	time_t t;
+	time(&t);
+	
+	srand((u_int)t);
+	for(u_int i = 0; i < len; ++i) array[i] = rand() % 100;
+}
+
+u_int sort(u_int *array, u_int len) {
+	u_int steps = 0;
+	for(u_int i = 0; i < len; ++i) {
+		for(u_int j = 0; j < len; ++j) {
+			for(u_int k = 0; k < len; ++k) printf("%d ", array[k]);
+			printf("\n");
+			if(array[j] > array[j + 1]) {
+				u_int swp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = swp;
+				steps++;
+			}
+		}
+	}
+	return steps;
+}
+
+int main() {
+	u_int array[10];
+	u_int len = sizeof(array) / sizeof(u_int);
+	
+	fill(array, len);
+	printf("\ntotal steps: %d\n", sort(array, len));
+	for(u_int i = 0; i < sizeof(array)/4; ++i) printf("%d ", array[i]);
+	printf("\n");
+	
+	
+	return 0;
+}
