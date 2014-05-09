312052893e66770f466efd1c23a3d5a2302d52f8
shawnstein <sherman@viacobra.de>
2014-05-09 17:57:15 +0200

    formatierung und optimierung

--- ADS_Prog1/main.c
+++ ADS_Prog1/main.c
@@ -1,3 +1,9 @@
+/*
+git add * && git commit -m "Kommentar" && git push origin master
+cc /home/sandor/Dokumente/GitHub/HTWK/ADS_Prog1/main.c -g -std=c99 && gdb ./home/sandor/Dokumente/GitHub/HTWK/ADS_Prog1/a.out
+cc /home/sandor/Dokumente/GitHub/HTWK/ADS_Prog1/main.c -g -std=c99 && ./home/sandor/Dokumente/GitHub/HTWK/ADS_Prog1/a.out
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
@@ -35,9 +41,6 @@ int main() {
 
 	fill(array, len);
 	printf("\ntotal steps: %d\n", sort(array, len));
-	for(u_int i = 0; i < sizeof(array)/4; ++i) printf("%d ", array[i]);
-	printf("\n");
-	
 	
 	return 0;
 }
