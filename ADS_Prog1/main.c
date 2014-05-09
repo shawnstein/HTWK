d4e276c24daa002557bc8b92c1e44e065015e6cf
shawnstein <sherman@viacobra.de>
2014-05-09 18:44:24 +0200

    neue ordner struktur

--- ADS_Prog1/main.c
+++ ADS_Prog1/main.c
@@ -1,7 +1,7 @@
 /*
 git add * && git commit -m "Kommentar" && git push origin master
 cc /home/sandor/Dokumente/GitHub/HTWK/ADS_Prog1/main.c -g -std=c99 && gdb ./home/sandor/Dokumente/GitHub/HTWK/ADS_Prog1/a.out
-cc /home/sandor/Dokumente/GitHub/HTWK/ADS_Prog1/main.c -g -std=c99 && ./home/sandor/Dokumente/GitHub/HTWK/ADS_Prog1/a.out
+cc /home/sandor/Dokumente/GitHub/HTWK/ADS_Prog1/main.c -g -std=c99 -o && ./home/sandor/Dokumente/GitHub/HTWK/ADS_Prog1/a.out
 */
 
 #include <stdio.h>
