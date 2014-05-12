41ab0f96d9f134b53f224cdeb57d7527422710bb
shawnstein <sherman@viacobra.de>
2014-05-12 13:52:49 +0200

    ads_progaufgabe erstellt

---
+++ ADS/adsaufgabe1/src/adsaufgabe1/adsaufgabe1/src/adsaufgabe1/IBubblesort.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package adsaufgabe1;
+
+/**
+ *
+ * @author Sandor
+ */
+public interface IBubblesort {
+    public int[] sortiere(String[] eingabe);
+}
