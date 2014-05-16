227e7d6cf18e80f4e2d1c7731c49f255f5177c86
shawnstein <sherman@viacobra.de>
2014-05-17 00:42:18 +0200

    matrix_mult
    create & open work

---
+++ AOP/Uebung_3/matrix_mult/nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=GNU-Linux-x86
+CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-Linux-x86
+CND_ARTIFACT_NAME_Debug=matrix_mult
+CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-Linux-x86/matrix_mult
+CND_PACKAGE_DIR_Debug=dist/Debug/GNU-Linux-x86/package
+CND_PACKAGE_NAME_Debug=matrixmult.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/GNU-Linux-x86/package/matrixmult.tar
+# Release configuration
+CND_PLATFORM_Release=GNU-Linux-x86
+CND_ARTIFACT_DIR_Release=dist/Release/GNU-Linux-x86
+CND_ARTIFACT_NAME_Release=matrix_mult
+CND_ARTIFACT_PATH_Release=dist/Release/GNU-Linux-x86/matrix_mult
+CND_PACKAGE_DIR_Release=dist/Release/GNU-Linux-x86/package
+CND_PACKAGE_NAME_Release=matrixmult.tar
+CND_PACKAGE_PATH_Release=dist/Release/GNU-Linux-x86/package/matrixmult.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
