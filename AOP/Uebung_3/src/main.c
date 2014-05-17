168b9d9eccf4f4ae04a30bbbcfe3d74a9bcf8cff
shawnstein <sherman@viacobra.de>
2014-05-17 13:08:21 +0200

    update new algo

--- AOP/Uebung_3/src/main.c
+++ AOP/Uebung_3/src/main.c
@@ -2,49 +2,74 @@
  * File:   main.c
  * Author: Sandor
  *
- * Created on 16. Mai 2014, 16:20
+ * Created on 17. Mai 2014, 10:59
  */
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <assert.h>
 #include "main.h"
 
-void multiplyMatrix(int **matrixA, matrix *ptr) {
+void printMatrix(int **matrix, coord *ptr) {
+    int i, j;
 
-    uint row = 0, column = 0;
-    matrixA[0][0] = 1;
-    printf("%d", matrixA[0][0]);
-    for(row; row < ptr[0].row; row++) {
-        for(column; column < ptr[0].column; column++) {    
-            matrixA[1][1] = 1;
-        }
+    for(i = 0; i < ptr[0].dimX; i++) {
+        for(j = 0; j < ptr[0].dimY; j++) {
+            printf("Wert: %d ", matrix[i][j]);
+        } printf("\n");
     }
 }
 
-void createMatrix() {
-    matrix *ptr = (matrix *) calloc(2, sizeof(matrix));
+void useMatrix(int **matrixA, int **matrixB, coord *ptr) {
+    int i, j;
     
-    if(ptr != NULL) {    
-        printf("Spaltenanzahl der Matrix A >> ");
-        scanf("%i", &ptr[0].column);
-        printf("Zeilenanzahl der Matrix A >> ");
-        scanf("%i", &ptr[0].row);
-        printf("Spaltenanzahl der Matrix B >> ");
-        scanf("%d", &ptr[1].column);
-        printf("Zeilenanzahl der Matrix B >> ");
-        scanf("%d", &ptr[1].row);   
-        int **dynArray = (int**) calloc(ptr[0].row, ptr[0].column);
-        if(dynArray != NULL) {
-            printf("test");
-            multiplyMatrix(dynArray, ptr);
-
-            free((void*)dynArray);
+    for(i = 0; i < ptr[0].dimX; i++) {
+        for(j = 0; j < ptr[0].dimY; j++) {
+            matrixA[i][j] = 2;
         }
-    }   
+    }
+
+    printf("Drucke Matrix... \n");
+    printMatrix(matrixA, ptr);
 }
 
-int main() {
-    createMatrix();
+int getValues() {
+    int value = 0;
+    printf("\nWert der Matrix: ");
+    scanf("%d", &value);
     
-    return 0;
+    return value;
 }
+
+int main(int argc, char** argv) {
+    int **matrixA, **matrixB, i = 0;
+   
+    coord *ptr = (coord *) calloc(2, sizeof(coord));
+    if(ptr != NULL) {
+        /*Erstellen Matrix A*/
+        printf("X-Groesse fuer A: ");
+        scanf("%i", &ptr[0].dimX);
+        printf("Y-Groesse fuer A: ");
+        scanf("%i", &ptr[0].dimY);
+
+        matrixA = (int **) calloc(ptr[0].dimX, sizeof(int *));
+        assert(*matrixA == NULL);
+        for(i; i < ptr[0].dimX; i++) matrixA[i] = (int *) calloc(ptr[0].dimY, sizeof(int));
+        /* Matrix A fertig*/
+        
+        /*Erstellen Matrix B*/
+        printf("X-Groesse fuer B: ");
+        scanf("%i", &ptr[1].dimX);
+        printf("Y-Groesse fuer B: ");
+        scanf("%i", &ptr[1].dimY);
+
+        matrixB = (int **) calloc(ptr[1].dimX, sizeof(int *));
+        assert(*matrixB == NULL);
+        for(i; i < ptr[1].dimX; i++) matrixB[i] = (int *) calloc(ptr[1].dimY, sizeof(int));
+        /* Matrix B fertig*/
+    }
+    useMatrix(matrixA, matrixB, ptr);
+
+    return (EXIT_SUCCESS);
+}
+
