05d5b62ff5f888fdb6b45c2d11a508e2f89e93eb
shawnstein <sherman@viacobra.de>
2014-05-18 17:51:53 +0200

    update

--- ADS/adsaufgabe1/src/adsaufgabe1/adsaufgabe1/src/adsaufgabe1/Bubblesort_MeinName.java
+++ ADS/adsaufgabe1/src/adsaufgabe1/adsaufgabe1/src/adsaufgabe1/Bubblesort_MeinName.java
@@ -5,7 +5,16 @@ public class Bubblesort_MeinName implements IBubblesort {
     public int[] sortiere(String[] eingabe) {
 	// Hier steht Ihr Bubblesort, das nebenbei die Permutation berechnet
 	// Bitte keine Ausgaben per Print hier einfuegen
-        
+        for (int i = 0, temp; i < eingabe.length-1; i++) {
+            for(int j = 0; j < eingabe.length-1; j++) {
+                 if (eingabe[j+1] < eingabe[j]) {
+                 } else {
+                     temp = eingabe[j];
+                     eingabe[j] = eingabe[j+1];
+                     eingabe[j+1] = temp;
+                }
+            }
+        }
 	// Ich liefere eine falsche Ausgabe
 	int[] erg = new int[eingabe.length];
 	return erg;
