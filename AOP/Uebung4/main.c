95d757466e1d4fa002d5c9e3eb851a24cb2e1d49
shawnstein <sherman@viacobra.de>
2014-05-21 20:07:50 +0200

    Uebung4 erstellt + Release

---
+++ AOP/Uebung4/main.c
@@ -0,0 +1,32 @@
+/* 
+ * File:   main.c
+ * Author: Sandor
+ *
+ * Created on 21. Mai 2014, 20:05
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define IN 1
+#define OUT 0
+
+int main(int argc, char** argv) {
+    int c, n1, nw, nc, state;
+    
+    state = OUT;
+    n1 = nw = nc = 0;
+    while((c = getchar()) != EOF) {
+        ++nc;
+        if(c == '\n') ++n1;
+        if(c == ' ' || c == '\t') state = OUT;
+        else if(state == OUT) {
+            state = IN;
+            ++nw;
+        }
+    }
+    printf("%d Zeilenumbrueche %d Woerter %d Zeichen\n", n1, nw, nc);
+
+    return (EXIT_SUCCESS);
+}
+
